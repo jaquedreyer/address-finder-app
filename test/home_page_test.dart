25b3258867d1e5c5b9ba6f56998db059c2a014f5
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-07-17 17:57:25 -0300

    test: implement TDD for the home page widgets

---
+++ test/home_page_test.dart
@@ -0,0 +1,18 @@
+import 'package:cep_finder/presentation/view/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Widget makeSut() => const MaterialApp(home: HomePage());
+
+// Teste de validaçao de componenetes (ambiente)
+  testWidgets('Verify if the home page contains all required widgets',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(makeSut());
+
+    expect(find.byType(Image), findsOneWidget);
+    expect(find.byType(TextFormField), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsOneWidget);
+    expect(find.byType(Text), findsOneWidget);
+  });
+}

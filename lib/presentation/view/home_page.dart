f991e81aa071cb657de64f77dcdce18de007ae1d
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-07-17 21:29:31 -0300

    feat: add home page widgets layout

--- lib/presentation/view/home_page.dart
+++ lib/presentation/view/home_page.dart
@@ -8,16 +8,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(
-            'Busca endereço'),
-      ),
-      body: const Placeholder()
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Busca endereço'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/location_pin.png',
+                  height: 76,
+                  width: 76,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'CEP',
+                        prefixIcon: Icon(Icons.map_outlined))),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(onPressed: () {}, child: const Text('Consultar'))
+              ],
+            ),
+          ),
+        ));
   }
 }

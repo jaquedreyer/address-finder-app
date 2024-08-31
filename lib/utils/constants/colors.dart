29f48c577a1bbb54a3d8828394a9e12993d21b74
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-08-31 17:36:16 -0300

    feat: add colors and strings as constants

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class CustomColors {
   static const Color backgroundColor = Color(0xFFF45558);
+  static const Color appBarColor = Color(0xFFFFC45C);
 }
--- lib/utils/constants/strings.dart
+++ lib/utils/constants/strings.dart
@@ -1,11 +1,16 @@
- class StringsConstants {
+class StringsConstants {
   // Titles
   static const cep = 'CEP';
+  static const appTitle = 'Consulta CEP e endereço';
 
 // Validators
-static const cepValido = 'Preencha com um CEP válido';
-static const cep8digitos = 'CEP inválido. O CEP precisa conter 8 dígitos';
+  static const cepValido = 'Preencha com um CEP válido';
+  static const cep8digitos = 'CEP inválido. O CEP precisa conter 8 dígitos';
+  static const cepInfo = 'Informações do CEP: ';
 
   // Buttons
   static const search = 'Consultar';
- }
+
+  // Errors
+  static const errorMessage = 'Erro ao buscar o endereço';
+}
